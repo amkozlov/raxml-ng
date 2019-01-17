a544adbcdfdc2d8c410c488115d2673cb0f57689
Sarah Lutteropp <sarah.lutteropp@gmail.com>
2019-01-17 19:10:54 +0100

    gave up, went back to the last correct commit, trying to re-add improvements from there.

--- src/bootstrap/VPTree.hpp
+++ src/bootstrap/VPTree.hpp
@@ -61,44 +61,30 @@ public:
 		_root = buildFromPoints(0, _items.size());
 	}
 
-	void search(pll_split_t target, int k, std::vector<unsigned int>* results, std::vector<double>* distances, unsigned int p) {
-		std::priority_queue<HeapItem> heap;
-
-		_tau = std::numeric_limits<unsigned int>::max();
-		search(_root, target, k, heap, p);
-
-		results->clear();
-		distances->clear();
-
-		while (!heap.empty()) {
-			results->push_back(_items[heap.top().index]);
-			distances->push_back(heap.top().dist);
-			heap.pop();
-		}
-
-		std::reverse(results->begin(), results->end());
-		std::reverse(distances->begin(), distances->end());
-	}
-
 	unsigned int search_mindist(pll_split_t target, unsigned int p) {
-		std::priority_queue<HeapItem> heap;
 
 		_tau = std::numeric_limits<unsigned int>::max();
-		search(_root, target, 1, heap, p);
+		search(_root, target, p);
 
-		return std::min(heap.top().dist, p - 1);
-
-		/*size_t min_idx = 0;
+		/*
+		 // check if the result is correct
+		 size_t min_idx = 0;
 		 unsigned int min = std::numeric_limits<unsigned int>::max();
 		 for (size_t i = 0; i < _items.size(); ++i) {
-		 unsigned int dist = distance(target, _splits[i], _split_len);
+		 unsigned int dist = distance(target, _splits[i], _split_len, _nTax);
 		 if (dist < min) {
 		 min = dist;
 		 min_idx = i;
 		 }
 		 }
-		 //std::cout << "   Sarah " << min << " for witness: " << split_string(_splits[min_idx]) << " and query: " << split_string(target) << "\n";
-		 return min;*/
+		 if (heap.top().dist != min) {
+		 std::cout << "ERROR!!! THE RESULT IS WRONG!!!\n";
+		 std::cout << "heap.top().dist: " << heap.top().dist << "\n";
+		 std::cout << "min: " << min << "\n";
+		 std::cout << "p-1: " << p - 1 << "\n";
+		 }*/
+
+		return std::min(_tau, p - 1);
 	}
 
 private:
@@ -140,8 +126,9 @@ private:
 		return hdist;
 	}
 
-	static unsigned int distance(pll_split_t s1, pll_split_t s2, unsigned int split_len, unsigned int min_hdist, unsigned int nTax) {
-		unsigned int dist = split_hamming_distance_lbound(s1, s2, split_len, min_hdist);
+	static unsigned int distance(pll_split_t s1, pll_split_t s2, unsigned int split_len, unsigned int nTax,
+			unsigned int max_interesting_distance) {
+		unsigned int dist = split_hamming_distance_lbound(s1, s2, split_len, max_interesting_distance);
 		return std::min(dist, nTax - dist);
 	}
 
@@ -165,17 +152,6 @@ private:
 			delete right;
 		}
 	}* _root;
-
-	struct HeapItem {
-		HeapItem(int index, unsigned int dist) :
-				index(index), dist(dist) {
-		}
-		int index;
-		unsigned int dist;
-		bool operator<(const HeapItem& o) const {
-			return dist < o.dist;
-		}
-	};
 
 	struct DistanceComparator {
 		const pll_split_t item;
@@ -201,8 +177,8 @@ private:
 		if (upper - lower > 1) {
 
 			// choose an arbitrary point and move it to the start
-			int i = (int) ((double) rand() / RAND_MAX * (upper - lower - 1)) + lower;
-			std::swap(_items[lower], _items[i]);
+			//int i = (int) ((double) rand() / RAND_MAX * (upper - lower - 1)) + lower;
+			std::swap(_items[lower], _items[upper - 1]);
 
 			int median = (upper + lower) / 2;
 
@@ -221,8 +197,8 @@ private:
 		return node;
 	}
 
-	void search(Node* node, pll_split_t target, unsigned int k, std::priority_queue<HeapItem>& heap, unsigned int p) {
-		if (node == NULL)
+	void search(Node* node, pll_split_t target, unsigned int p) {
+		if (node == NULL || _tau == 1)
 			return;
 
 		unsigned int minDist;
@@ -233,6 +209,16 @@ private:
 		}
 		minDist = std::min(minDist, _nTax - minDist);
 
+		if (node->left == NULL && node->right == NULL) { // both children are NULL, we only need to check if dist < _tau
+			if (minDist < _tau) {
+				unsigned int dist = distance(_splits[_items[node->index]], target, _split_len, _nTax, _tau);
+				if (dist < _tau) {
+					_tau = dist;
+				}
+			}
+			return;
+		}
+
 		if (minDist >= node->threshold) { // interesting stuff happens...
 			bool distComputed = false;
 			unsigned int dist;
@@ -241,64 +227,63 @@ private:
 				dist = distance(_splits[_items[node->index]], target, _split_len, _nTax);
 				distComputed = true;
 				if (dist < _tau) {
-					if (heap.size() == k)
-						heap.pop();
-					heap.push(HeapItem(node->index, dist));
-					if (heap.size() == k)
-						_tau = heap.top().dist;
+					_tau = dist;
 				}
 			}
 
-			if (node->left == NULL && node->right == NULL) {
-				return;
-			}
-
-			if (minDist + _tau >= node->threshold) {
-				search(node->right, target, k, heap, p);
-			}
-
-			if (minDist <= node->threshold + _tau) {
-				if (!distComputed) {
-					dist = distance(_splits[_items[node->index]], target, _split_len, _nTax); // do we need the dist computation here?
+			if (node->right == NULL) {
+				if (minDist <= node->threshold + _tau) {
+					if (!distComputed) {
+						dist = distance(_splits[_items[node->index]], target, _split_len, _nTax, node->threshold + _tau); // do we need the dist computation here?
+					}
+					if (dist <= node->threshold + _tau) {
+						search(node->left, target, p);
+					}
 				}
-				if (dist <= node->threshold + _tau) {
-					search(node->left, target, k, heap, p);
+			} else if (node->left == NULL) {
+				if (minDist + _tau >= node->threshold) {
+					search(node->right, target, p);
+				}
+			} else {
+				if (minDist + _tau >= node->threshold) {
+					search(node->right, target, p);
+				}
+
+				if (minDist <= node->threshold + _tau) {
+					if (!distComputed) {
+						dist = distance(_splits[_items[node->index]], target, _split_len, _nTax, node->threshold + _tau); // do we need the dist computation here?
+					}
+					if (dist <= node->threshold + _tau) {
+						search(node->left, target, p);
+					}
 				}
 			}
 			return;
 		}
 
-		unsigned int dist = distance(_splits[_items[node->index]], target, _split_len, _nTax);
+		unsigned int dist = distance(_splits[_items[node->index]], target, _split_len, _nTax, node->threshold + _tau);
 		//printf("dist=%g tau=%gn", dist, _tau );
 
 		if (dist < _tau) {
-			if (heap.size() == k)
-				heap.pop();
-			heap.push(HeapItem(node->index, dist));
-			if (heap.size() == k)
-				_tau = heap.top().dist;
-		}
-
-		if (node->left == NULL && node->right == NULL) {
-			return;
+			_tau = dist;
 		}
 
 		if (dist < node->threshold) {
 			if (dist <= node->threshold + _tau) {
-				search(node->left, target, k, heap, p);
+				search(node->left, target, p);
 			}
 
 			if (dist + _tau >= node->threshold) {
-				search(node->right, target, k, heap, p);
+				search(node->right, target, p);
 			}
 
 		} else {
 			if (dist + _tau >= node->threshold) {
-				search(node->right, target, k, heap, p);
+				search(node->right, target, p);
 			}
 
 			if (dist <= node->threshold + _tau) {
-				search(node->left, target, k, heap, p);
+				search(node->left, target, p);
 			}
 		}
 	}
