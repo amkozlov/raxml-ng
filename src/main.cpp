64a5810b9659be1ccaf57db33af2d867581382e8
Sarah Lutteropp <sarah.lutteropp@gmail.com>
2019-06-12 15:33:32 +0200

    uncommented some code

--- src/main.cpp
+++ src/main.cpp
@@ -1813,7 +1813,7 @@ void print_final_output(const RaxmlInstance& instance, const Checkpoint& checkp)
         		TBEExtraTableStream tbeExtraTable(extra_table_file);
         		tbeExtraTable << *dynamic_cast<const TransferBootstrapTree*>(it.second.get());
         	}
-        	/*if (opts.tbe_extra_array) {
+        	if (opts.tbe_extra_array) {
         	    auto extra_array_file = opts.tbe_extra_array_file();
         	    TBEExtraArrayStream tbeExtraArray(extra_array_file);
         	    tbeExtraArray << dynamic_cast<TransferBootstrapTree&>(it.second);
@@ -1822,7 +1822,7 @@ void print_final_output(const RaxmlInstance& instance, const Checkpoint& checkp)
         	    auto extra_tree_file = opts.tbe_extra_tree_file();
         	    TBEExtraTreeStream tbeExtraTree(extra_tree_file);
         	    tbeExtraTree << dynamic_cast<const TransferBootstrapTree&>(*it.second);
-        	}*/
+        	}
         }
       }
     }
