b87c1369d8b16c5609e370d6db613467338c84c6
Sarah Lutteropp <sarah.lutteropp@gmail.com>
2019-06-12 18:17:54 +0200

    changed extra taxa table

--- src/io/TBEExtraArrayStream.cpp
+++ src/io/TBEExtraArrayStream.cpp
@@ -6,9 +6,9 @@ using namespace std;
 TBEExtraArrayStream& operator<<(TBEExtraArrayStream& stream, const TransferBootstrapTree& tree)
 {
   pllmod_tbe_extra_info_t* extra_info = tree.get_extra_info();
-  stream << "taxon_id;taxon_transfer_stat\n";
+  stream << "taxon_label;taxon_transfer_stat\n";
   for (size_t i = 0; i < tree.num_tips(); ++i) {
-	  stream << i << ";" << (double) (extra_info->extra_taxa_array[i]) / extra_info->num_close_enough_branches << "\n";
+	  stream << tree.tip_labels_list()[i] << ";" << (double) (extra_info->extra_taxa_array[i]) / (double) extra_info->num_bs_trees << "\n";
   }
   return stream;
 }
