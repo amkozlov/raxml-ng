f9ba4f9eeacd5bbafa05d3fc609637925b428490
Sarah Lutteropp <sarah.lutteropp@gmail.com>
2019-06-17 18:37:18 +0200

    made tbe extra table output more like in booster

--- src/io/TBEExtraArrayStream.cpp
+++ src/io/TBEExtraArrayStream.cpp
@@ -6,9 +6,9 @@ using namespace std;
 TBEExtraArrayStream& operator<<(TBEExtraArrayStream& stream, const TransferBootstrapTree& tree)
 {
   pllmod_tbe_extra_info_t* extra_info = tree.get_extra_info();
-  stream << "taxon_label;taxon_transfer_stat\n";
+  stream << "Taxon\ttIndex\n";
   for (size_t i = 0; i < tree.num_tips(); ++i) {
-	  stream << tree.tip_labels_list()[i] << ";" << (double) (extra_info->extra_taxa_array[i] * 100) / (double) extra_info->num_bs_trees << "\n";
+	  stream << tree.tip_labels_list()[i] << "\t" << (double) (extra_info->extra_taxa_array[i] * 100) / (double) extra_info->num_bs_trees << "\n";
   }
   return stream;
 }
--- src/io/TBEExtraTableStream.cpp
+++ src/io/TBEExtraTableStream.cpp
@@ -13,10 +13,20 @@ TBEExtraTableStream& operator<<(TBEExtraTableStream& stream, const TransferBoots
 	  throw std::runtime_error("Extra taxa table is null");
   }
   double divideBy = extra_info->num_bs_trees * tree.num_splits();
+  stream << "Edge\tSupport\t";
   for (size_t i = 0; i < tree.num_tips(); ++i) {
-	  for (size_t j = 0; j < tree.num_splits(); ++j) {
-		  stream << fixed << std::setprecision(6) << (double) (extra_info->extra_taxa_table[j][i]) / divideBy;
-		  if (j < tree.num_splits() - 1) {
+	  stream << tree.tip_labels_list()[i];
+	  if (i < tree.num_tips() - 1) {
+		  stream << "\t";
+	  } else {
+		  stream << "\n";
+	  }
+  }
+  for (size_t i = 0; i < tree.num_splits(); ++i) {
+	  stream << tree.get_split_node_map()[i]->clv_index << "\t" << "???" << "\t";
+	  for (size_t j = 0; j < tree.num_tips(); ++j) {
+		  stream << fixed << std::setprecision(6) << (double) (extra_info->extra_taxa_table[i][j]) / divideBy;
+		  if (j < tree.num_tips() - 1) {
 			  stream << "\t";
 		  }
 	  }
