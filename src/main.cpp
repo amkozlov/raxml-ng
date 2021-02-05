7f46f2a8f0c3e783d324f6b3022fed35b3d41159
Alexey Kozlov <alexey.kozlov@h-its.org>
2021-02-05 16:25:38 +0100

    consistently use autoref in foreach loops to prevent unnecessary copies and clang warnings

--- src/main.cpp
+++ src/main.cpp
@@ -519,7 +519,7 @@ bool check_msa(RaxmlInstance& instance)
   if (!parted_msa_view.taxon_name_map().empty())
   {
     LOG_ERROR << endl;
-    for (auto it: parted_msa_view.taxon_name_map())
+    for (auto& it: parted_msa_view.taxon_name_map())
       LOG_ERROR << "ERROR: Following taxon name contains invalid characters: " << it.first << endl;
 
     LOG_ERROR << endl;
@@ -1263,7 +1263,7 @@ void load_checkpoint(RaxmlInstance& instance, CheckpointManager& cm)
     if (instance.bootstop_checker)
     {
       auto bs_tree = instance.random_tree;
-      for (auto it: ckpfile.bs_trees)
+      for (auto& it: ckpfile.bs_trees)
       {
         bs_tree.topology(it.second.second);
 
@@ -1796,7 +1796,7 @@ void draw_bootstrap_support(RaxmlInstance& instance, Tree& ref_tree,
         assert(0);
 
       Tree tree = ref_tree;
-      for (auto bs: bs_trees)
+      for (auto& bs: bs_trees)
       {
         tree.topology(bs);
         sup_tree->add_replicate_tree(tree);
@@ -1837,7 +1837,7 @@ bool check_bootstop(const RaxmlInstance& instance, const TreeTopologyList& bs_tr
   Tree bs_tree = instance.random_tree;
   size_t bs_num = 0;
   bool converged = false;
-  for (auto it: bs_trees)
+  for (auto& it: bs_trees)
   {
     bs_tree.topology(it);
 
@@ -2121,7 +2121,7 @@ void check_terrace(const RaxmlInstance& instance, const Tree& tree)
 void save_ml_trees(const Options& opts, const CheckpointFile& checkp)
 {
   NewickStream nw(opts.ml_trees_file(), std::ios::out);
-  for (auto topol: checkp.ml_trees)
+  for (auto& topol: checkp.ml_trees)
   {
     Tree ml_tree = checkp.tree();
     ml_tree.topology(topol.second.second);
@@ -2316,7 +2316,7 @@ void print_final_output(const RaxmlInstance& instance, const CheckpointFile& che
   //    NewickStream nw(opts.bootstrap_trees_file(), std::ios::out | std::ios::app);
       NewickStream nw(opts.bootstrap_trees_file(), std::ios::out);
 
-      for (auto topol: checkp.bs_trees)
+      for (auto& topol: checkp.bs_trees)
       {
         Tree bs_tree = checkp.tree();
         bs_tree.topology(topol.second.second);
