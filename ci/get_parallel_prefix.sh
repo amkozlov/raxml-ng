17f930c8d65b141b713b54f6c37ae603c5e86a30
Alexey Kozlov <alexey.kozlov@h-its.org>
2022-12-20 19:20:53 +0100

    Jenkins: improve parallel config label

--- ci/get_parallel_prefix.sh
+++ ci/get_parallel_prefix.sh
@@ -1,2 +1,35 @@
 #!/bin/bash
-echo ${1//[^[:alnum:]]/}
+
+POSITIONAL_ARGS=()
+
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -np|--np)
+      RANKS="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    -t|--threads)
+      THREADS="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    -w|--workers)
+      WORKERS="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    -*|--*)
+      echo "Unknown option $1"
+      exit 1
+      ;;
+    *)
+      POSITIONAL_ARGS+=("$1") # save positional arg
+      shift # past argument
+      ;;
+  esac
+done
+
+let CPU=RANKS*THREADS
+
+echo "C${CPU}___R${RANKS}_T${THREADS}_W${WORKERS}"
