2725dafb28f5c64dc7d4e19438b0d5ca29cfd436
Alexey Kozlov <alexey.kozlov@h-its.org>
2023-12-22 15:34:50 +0100

    update version

--- src/version.h
+++ src/version.h
@@ -1,3 +1,3 @@
-#define RAXML_VERSION "1.2.0-dev"
-#define RAXML_DATE    "03.12.2023"
-#define RAXML_INTVER  120
+#define RAXML_VERSION "1.2.1-master"
+#define RAXML_DATE    "22.12.2023"
+#define RAXML_INTVER  121
