93a2f9649b72a543ddc88fe26edec95e04cda24f
togkousa <togkousa@gmail.com>
2024-12-11 12:36:14 +0000

    switched printout of stopping criteria into LOG_DEBUG

--- src/Optimizer.cpp
+++ src/Optimizer.cpp
@@ -446,7 +446,7 @@ double Optimizer::optimize_topology_adaptive(TreeInfo& treeinfo, CheckpointManag
       ParallelContext::barrier();
 
       _lh_epsilon = _criterion->get_epsilon(ParallelContext::group_id());
-      LOG_PROGRESS(loglh) << approach << " apporach. Epsilon = " << _lh_epsilon << endl;
+      LOG_DEBUG << approach << " apporach. Epsilon = " << _lh_epsilon << endl;
 
       if(ParallelContext::group_master_thread())
         cm.set_epsilon(_lh_epsilon);
@@ -494,13 +494,13 @@ double Optimizer::optimize_topology_adaptive(TreeInfo& treeinfo, CheckpointManag
           
           double p_value = _criterion->get_pvalue(ParallelContext::group_id());
           epsilon = _criterion->get_epsilon(ParallelContext::group_id()); 
-          LOG_PROGRESS(loglh) << "KH multiple-testing epsilon = " << epsilon << endl;
+          LOG_DEBUG << "KH multiple-testing epsilon = " << epsilon << endl;
           impr = ((loglh - old_loglh > epsilon) && (p_value < 1));
           
         } else {
 
           epsilon = _criterion->get_epsilon(ParallelContext::group_id());
-          LOG_PROGRESS(loglh) << "KH criterion epsilon = " << epsilon << endl;
+          LOG_DEBUG << "KH criterion epsilon = " << epsilon << endl;
           impr = (loglh - old_loglh > epsilon);
         }
       } else {
@@ -571,13 +571,13 @@ double Optimizer::optimize_topology_adaptive(TreeInfo& treeinfo, CheckpointManag
           
           double p_value = _criterion->get_pvalue(ParallelContext::group_id());
           epsilon = _criterion->get_epsilon(ParallelContext::group_id()); 
-          LOG_PROGRESS(loglh) << "KH multiple-testing epsilon = " << epsilon << endl;
+          LOG_DEBUG << "KH multiple-testing epsilon = " << epsilon << endl;
           impr = ((loglh - old_loglh > epsilon) && (p_value < 1));
 
         } else {
 
           epsilon = _criterion->get_epsilon(ParallelContext::group_id());
-          LOG_PROGRESS(loglh) << "KH criterion epsilon = " << epsilon << endl;
+          LOG_DEBUG << "KH criterion epsilon = " << epsilon << endl;
           impr = (loglh - old_loglh > epsilon);
         }
       } else {
