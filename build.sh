0840389c7ea065635ff299450f9eff05e57cc000
Christoph Stelz <christoph.stelz@student.kit.edu>
2025-02-10 15:48:21 +0000

    Bump bts version

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-binarytreesummation_version=2ceac66c9269a3dfe8956403d8e4edb8b86e62c4
+binarytreesummation_version=ae0f58394902813ba4d86ecb0a5e0d26e47f93c5
 ${CMAKE:-cmake} -B cmake-build-release -S . -DUSE_MPI=ON -DBUILD_TESTS=OFF \
     -DREPRODUCIBLE=ON \
     -Dbinarytreesummation_version=$binarytreesummation_version \
