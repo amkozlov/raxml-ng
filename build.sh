1f2f7f6a7f30cfeb802b0fde15423a6438694210
Christoph Stelz <christoph.stelz@student.kit.edu>
2025-02-20 08:53:09 +0000

    Bump bts-c version
    Now use ReproBLAS Allreduce

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-binarytreesummation_version=ae0f58394902813ba4d86ecb0a5e0d26e47f93c5
+binarytreesummation_version=0e2cd01960c808942314867f7f73b24ead8e558d
 ${CMAKE:-cmake} -B cmake-build-release -S . -DUSE_MPI=ON -DBUILD_TESTS=OFF \
     -DREPRODUCIBLE=ON \
     -Dbinarytreesummation_version=$binarytreesummation_version \
