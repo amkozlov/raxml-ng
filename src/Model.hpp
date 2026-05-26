amkozlov/raxml-ng

commit 03bcfd0cf1f076af3c1c862d9f4b80fe9745e16b

moose: fix sporadic failures with [ALPHA] Worse log-likelihood after optimization

src/Model.hpp
@@ -153,7 +153,13 @@ public:
   unsigned int  num_free_params(int mask = CORAX_OPT_PARAM_ALL) const;
 
   /* setters */
-  void alpha(double value) { _alpha = value; };
+  void alpha(double value)
+  {
+    _alpha = value;
+    /* IMPORTANT: re-compute per-category rates with the new alpha value */
+    assert(_ratecat_rates.size() ==  _num_ratecats);
+    corax_compute_gamma_cats(_alpha, _num_ratecats, _ratecat_rates.data(), _gamma_mode);
+  };
   void pinv(double value) { _pinv = value; };
   void brlen_scaler(double value) { _brlen_scaler = value; };
   void base_freqs(size_t i, const doubleVector& value) { _submodels.at(i).base_freqs(value); };

src/version.h
@@ -1,3 +1,3 @@
 #define RAXML_VERSION "2.0.0-dev"
-#define RAXML_DATE    "07.04.2026"
+#define RAXML_DATE    "08.04.2026"
 #define RAXML_INTVER  200

commit 6ba0ebc85fcabb81b5111fddee8c762fad69ed4f

fix regression introduced by 03bcfd0cf1 (moose slowdown + incorrect FreeRate rates in the output, trees and logLH not affected)

src/Model.hpp
@@ -156,9 +156,13 @@ public:
   void alpha(double value)
   {
     _alpha = value;
+
     /* IMPORTANT: re-compute per-category rates with the new alpha value */
-    assert(_ratecat_rates.size() ==  _num_ratecats);
-    corax_compute_gamma_cats(_alpha, _num_ratecats, _ratecat_rates.data(), _gamma_mode);
+    if (ratehet_mode() == CORAX_UTIL_MIXTYPE_GAMMA)
+    {
+      assert(_ratecat_rates.size() ==  _num_ratecats);
+      corax_compute_gamma_cats(_alpha, _num_ratecats, _ratecat_rates.data(), _gamma_mode);
+    }
   };
   void pinv(double value) { _pinv = value; };
   void brlen_scaler(double value) { _brlen_scaler = value; };

src/TreeInfo.cpp
@@ -684,7 +684,8 @@ void assign(Model &model, const TreeInfo &treeinfo, size_t partition_id)
     return;
 
   assign(model, pll_treeinfo.partitions[partition_id]);
-  model.alpha(pll_treeinfo.alphas[partition_id]);
+  if (model.ratehet_mode() == CORAX_UTIL_MIXTYPE_GAMMA)
+    model.alpha(pll_treeinfo.alphas[partition_id]);
   if (pll_treeinfo.brlen_scalers)
     model.brlen_scaler(pll_treeinfo.brlen_scalers[partition_id]);
 }

src/version.h
@@ -1,3 +1,3 @@
 #define RAXML_VERSION "2.0.1-dev"
-#define RAXML_DATE    "20.05.2026"
+#define RAXML_DATE    "26.05.2026"
 #define RAXML_INTVER  201